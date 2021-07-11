2d199cbc48c5963b282c02e14e08019ca2e98d8a
Moeen Zamani <moeen.zamani@gmail.com>
2021-07-11 19:19:39 +0430

    chore(dataart-sample-phone): add sample application for phones and tablet

--- dataart-sample-phone/src/main/java/com/datartproject/dataart_sample_phone/MainActivity.kt
+++ dataart-sample-phone/src/main/java/com/datartproject/dataart_sample_phone/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.datartproject.dataart_sample_phone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.datartproject.dataart.client.DataArt
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var dataart: DataArt
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dataart = DataArt(this, DataArt.Config("your-api-key"))
+    }
+
+    fun onButtonClicked(v: View) {
+        if (v.id == R.id.btn) {
+            dataart.emitAction(
+                "some-event-key",
+                "some-user-key",
+                false,
+                Date(),
+                mapOf()
+            )
+        }
     }
 }
